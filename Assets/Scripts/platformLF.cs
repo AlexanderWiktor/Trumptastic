906cbe66514adf7c55a32a495a1a0e143e70ee66
AlexanderWiktor <alexk8899@gmail.com>
2016-04-11 13:05:54 +0200

    Platforms moving and shit

---
+++ Assets/Scripts/platformLF.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class platformLF : MonoBehaviour {
+   
+public float speed;
+public float maxX;
+public float minX;
+enum Direction
+{
+   LEFT, RIGHT
+};
+private Direction state;
+	// Use this for initialization
+	void Start () {
+state = Direction.LEFT;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+if (state == Direction.RIGHT){
+    
+if (transform.position.x>maxX)
+{
+   transform.Translate(Vector2.right * -speed * Time.deltaTime);
+  state = Direction.LEFT;
+}
+else
+{
+       transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+}}
+if (state == Direction.LEFT)
+{
+    if (transform.position.x<minX)
+    {
+               transform.Translate(Vector2.right * speed * Time.deltaTime);
+state = Direction.RIGHT;
+    }
+    else
+    {
+           transform.Translate(Vector2.right * -speed * Time.deltaTime);
+
+    }
+}
+	}
+}
---
+++ Assets/Scripts/platformUD.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class platformUD : MonoBehaviour {
+    
+public float speed;
+public float maxY;
+public float minY;
+enum Direction
+{
+    UP, DOWN
+};
+private Direction state;
+	// Use this for initialization
+	void Start () {
+state = Direction.UP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+if (state == Direction.UP){
+    
+if (transform.position.y>maxY)
+{
+   transform.Translate(Vector2.up * -speed * Time.deltaTime);
+  state = Direction.DOWN;
+}
+else
+{
+       transform.Translate(Vector2.up * speed * Time.deltaTime);
+
+}}
+if (state == Direction.DOWN)
+{
+    if (transform.position.y<minY)
+    {
+               transform.Translate(Vector2.up * speed * Time.deltaTime);
+state = Direction.UP;
+    }
+    else
+    {
+           transform.Translate(Vector2.up * -speed * Time.deltaTime);
+
+    }
+}
+	}
+}
