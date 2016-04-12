5adb549262933414c6d41426cd976fdf98de9433
AlexanderWiktor <alexk8899@gmail.com>
2016-04-12 21:33:58 +0200

    Uploading scene, background only moves left-right.

---
+++ Assets/Scripts/backgroundBeh.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backgroundBeh : MonoBehaviour {
+
+private GameObject camera;
+	// Use this for initialization
+	void Start () {
+	camera = GameObject.FindGameObjectWithTag("MainCamera");
+    
+   
+	}
+	
+	// Update is called once per frame
+	void Update () {
+  //  float targetX = transform.position.x;
+this.gameObject.transform.position = new Vector3(camera.gameObject.transform.position.x,this.gameObject.transform.position.y,this.gameObject.transform.position.z);
+	}
+}
